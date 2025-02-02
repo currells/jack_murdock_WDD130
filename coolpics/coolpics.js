3852fd26ac091a6f055b918cb7099f5006a22f56
JM-BYUI <johnfranklinmurdock@gmail.com>
2025-02-01 22:56:35 -0700

    modal

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -6,3 +6,37 @@ function toggleMenu() {
 }
 
 menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+    const menu = document.querySelector(".menu");
+    if (window.innerWidth > 1000) {
+      menu.classList.remove("hide");
+    } else {
+      menu.classList.add("hide");
+    }
+  }
+  
+handleResize();
+window.addEventListener("resize", handleResize);
+
+function openModal(event) {
+    const modal = document.querySelector(".viewer");
+    const modalImage = document.querySelector(".modal-image");
+    const clickedImageSrc = event.target.src;
+  
+    modal.style.display = "grid"; 
+    modalImage.src = clickedImageSrc;
+  }
+  
+  function closeModal() {
+    const modal = document.querySelector(".viewer");
+    modal.style.display = "none"; 
+  }
+  
+  const images = document.querySelectorAll("main .Gallery img");
+  images.forEach(image => {
+    image.addEventListener("click", openModal);
+  });
+  
+  const closeButton = document.querySelector(".close-viewer");
+  closeButton.addEventListener("click", closeModal);
